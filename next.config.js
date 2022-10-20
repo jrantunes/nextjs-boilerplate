7115d75a656a0fb1e59525dafafe29e42bb484e7
José neto <joseantunes2001@gmail.com>
2022-10-20 19:24:47 -0300

    fix next-pwa disabled

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
   dest: 'public',
-  disable: !process.env.NODE_ENV === 'production'
+  disable: !(process.env.NODE_ENV === 'production')
 })
 
 const nextConfig = withPWA({
